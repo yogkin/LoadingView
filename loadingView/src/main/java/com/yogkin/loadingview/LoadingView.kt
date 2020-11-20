9c870c057c7da511c6376e14ab347b9e6d0e5de4
czm <yogkin@163.com>
2020-11-20 17:16:35 +0800

    add cancel option

--- loadingView/src/main/java/com/yogkin/loadingview/LoadingView.kt
+++ loadingView/src/main/java/com/yogkin/loadingview/LoadingView.kt
@@ -1,17 +1,19 @@
 package com.yogkin.loadingview
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 
-class LoadingView(mContext: Context) {
+class LoadingView(mContext: Context, cancelAble: Boolean = false) {
 
+    @SuppressLint("InflateParams")
     private val view = LayoutInflater.from(mContext).inflate(R.layout.dialog_loading_base, null)
     private val contextText = view.findViewById<TextView>(R.id.tv_loading)
     private var mDialog = AlertDialog.Builder(mContext, R.style.custom_dialog_dim_disabled)
         .setView(view)
-        .setCancelable(false)
+        .setCancelable(cancelAble)
         .create()
 
     fun show() {
